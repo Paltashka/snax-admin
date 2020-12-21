ccd8db34e036af689ae9987f72a6e605fa5f641d
Andriy Yurchuk <paltashka50@gmail.com>
2020-12-21 11:55:07 +0200

    added tables

--- src/views/dashboards/Dashboard1.js
+++ src/views/dashboards/Dashboard1.js
@@ -1,71 +1,93 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import {Row, Col, Card, CardBody} from 'reactstrap';
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+import * as data from "../tables/DataBootstrapTable";
+import {upperCasePipe} from "../../components/helpers/upperCasePipe";
 
-import {
-    SalesOverview,
-    Visitors,
-    SocialCards,
-    NewsleterCompaign,
-    CardBandwidth,
-    CardDownload,
-    CardProfile,
-    CardContact,
-    CardCity,
-    TimeLine,
-    Feeds
-} from '../../components/dashboard/index.js';
+
+const options = {
+    // afterInsertRow: onAfterInsertRow,  // A hook for after insert rows
+    // afterDeleteRow: onAfterDeleteRow, // A hook for after droping rows.
+    // afterSearch: afterSearch, // define a after search hook
+};
+const selectRowProp = {
+    mode: "checkbox",
+};
+const cellEditProp = {
+    mode: "click",
+    blurToSave: true,
+};
+
+const headers = ['id', 'icon', 'name', 'locked', 'color template', 'number of skins', 'live', 'last update'];
+
+function jobNameValidator(value, row) {
+    // If this function return an object, you got some ability to customize your error message
+    const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+    if (!value) {
+        response.isValid = false;
+        response.notification.type = 'error';
+        response.notification.msg = 'Value must be inserted';
+        response.notification.title = 'Requested Value';
+    }
+    // else if (value.length < 10) {
+    //   response.isValid = false;
+    //   response.notification.type = 'error';
+    //   response.notification.msg = 'Value must have 10+ characters';
+    //   response.notification.title = 'Invalid Value';
+    // }
+    return response;
+}
 
 const FirstDashboard = () => {
     return (
         <div>
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row-1                                                                          */}
-            {/* --------------------------------------------------------------------------------*/}
             <Row>
-                <Col lg="8" md="12">
-                    <SalesOverview />
-                </Col>
-                <Col lg="4" md="12">
-                    <Visitors />
-                </Col>
-            </Row>
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row-2                                                                          */}
-            {/* --------------------------------------------------------------------------------*/}
-            <Row>
-                <Col md="12" xl="4" className="col-xlg-3">
-                    <Feeds />
-                </Col>
-                <Col md="12" xl="8" className="col-xlg-9">
-                    <NewsleterCompaign />
-                </Col>
-            </Row>
+                <Col md="12">
+                    <Card>
+                        <CardBody>
+                            <BootstrapTable
+                                striped
+                                hover
+                                condensed
+                                search={true}
+                                data={data.jsondata}
+                                deleteRow={true}
+                                selectRow={selectRowProp}
+                                // pagination
+                                insertRow={true}
+                                columnFilter={true}
+                                options={options}
+                                cellEdit={cellEditProp}
+                                tableHeaderClass="mb-0"
+                            >
 
-            <SocialCards />
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row-3                                                                          */}
-            {/* --------------------------------------------------------------------------------*/}
-            <Row>
-                <Col lg="4" md="12">
-                    <CardBandwidth />
-                </Col>
-                <Col lg="4" md="12">
-                    <CardDownload />
-                </Col>
-                <Col lg="4" md="12">
-                    <CardCity />
-                </Col>
-            </Row>
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row-4                                                                          */}
-            {/* --------------------------------------------------------------------------------*/}
-            <Row>
-                <Col md="12" xl="4" className="col-xlg-3">
-                    <CardProfile />
-                    <CardContact />
-                </Col>
-                <Col md="12" xl="8" className="col-xlg-9">
-                    <TimeLine />
+                                {headers.map((item, i)=>{
+                                    if(i === 0) {
+                                        return <TableHeaderColumn width="100" dataField={item}  dataSort={ true } isKey>
+                                            <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                                        </TableHeaderColumn>
+                                    }
+                                    if(item === "icon") {
+                                        return <TableHeaderColumn width="100" dataField={item}  dataFormat={(cell, format) => {
+                                            return <img src={cell} dataSort={ true }/>
+                                        } }>
+                                            <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                                        </TableHeaderColumn>
+                                    }
+
+                                    if(i >= 0 && i <= 6){
+                                        return <TableHeaderColumn width="100" dataField={item} dataSort={ true }  editable={{validator: jobNameValidator}}>
+                                            <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                                        </TableHeaderColumn>
+                                    }
+
+                                    return <TableHeaderColumn width="100" dataField={item}  dataSort={ true }>
+                                        <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                                    </TableHeaderColumn>
+                                })}
+                            </BootstrapTable>
+                        </CardBody>
+                    </Card>
                 </Col>
             </Row>
         </div>
--- src/views/tables/ReactBootstrapTable.js
+++ src/views/tables/ReactBootstrapTable.js
@@ -29,7 +29,7 @@ function onAfterInsertRow(row) {
 //   }
 // }
 const options = {
-  //afterInsertRow: onAfterInsertRow,  // A hook for after insert rows
+  // afterInsertRow: onAfterInsertRow,  // A hook for after insert rows
   // afterDeleteRow: onAfterDeleteRow, // A hook for after droping rows.
   // afterSearch: afterSearch, // define a after search hook
 };
@@ -43,24 +43,42 @@ const cellEditProp = {
 
 const headers = ['id', 'icon', 'name', 'locked', 'color template', 'number of skins', 'live', 'last update'];
 
+function jobNameValidator(value, row) {
+  // If this function return an object, you got some ability to customize your error message
+  const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+  if (!value) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be inserted';
+    response.notification.title = 'Requested Value';
+  }
+  // else if (value.length < 10) {
+  //   response.isValid = false;
+  //   response.notification.type = 'error';
+  //   response.notification.msg = 'Value must have 10+ characters';
+  //   response.notification.title = 'Invalid Value';
+  // }
+  return response;
+}
+
 const Datatables = () => {
 
-  const [allData, setAllData] = useState(data.jsondata);
-  const [selectedFilter, setFilter] = useState(null);
+  // const [allData, setAllData] = useState(data.jsondata);
+  // const [selectedFilter, setFilter] = useState(null);
 
-  useEffect(() => {
-    const arr = allData.concat().sort((a, b) => {
-      if (a[selectedFilter] < b[selectedFilter]) {
-        return -1;
-      }
-      if (a[selectedFilter] > b[selectedFilter]) {
-        return 1;
-      }
-      return 0;
-    });
-    setAllData(arr)
-    console.log(arr)
-  },[selectedFilter])
+  // useEffect(() => {
+  //   const arr = allData.concat().sort((a, b) => {
+  //     if (a[selectedFilter] < b[selectedFilter]) {
+  //       return -1;
+  //     }
+  //     if (a[selectedFilter] > b[selectedFilter]) {
+  //       return 1;
+  //     }
+  //     return 0;
+  //   });
+  //   setAllData(arr)
+  //   console.log(arr)
+  // },[selectedFilter])
 
   return (
     <div>
@@ -73,56 +91,39 @@ const Datatables = () => {
                 hover
                 condensed
                 search={true}
-                data={allData}
+                data={data.jsondata}
                 deleteRow={true}
                 selectRow={selectRowProp}
                 // pagination
                 insertRow={true}
+                columnFilter={true}
                 options={options}
                 cellEdit={cellEditProp}
                 tableHeaderClass="mb-0"
               >
+
                 {headers.map((item, i)=>{
                   if(i === 0) {
-                    return <TableHeaderColumn width="100" dataField={item}  isKey>
-                      <span onClick={() => {
-                        setFilter(item);
-                      }} style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                    return <TableHeaderColumn width="100" dataField={item}  dataSort={ true } isKey>
+                      <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
                     </TableHeaderColumn>
                   }
                   if(item === "icon") {
                     return <TableHeaderColumn width="100" dataField={item}  dataFormat={(cell, format) => {
-                        return <img src={cell}/>
+                        return <img src={cell} dataSort={ true }/>
                     } }>
-                      <span onClick={() => {
-                        setFilter(item);
-                      }} style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                      <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
                     </TableHeaderColumn>
                   }
 
                   if(i >= 0 && i <= 6){
-                    return <TableHeaderColumn width="100" dataField={item}   editable={{
-                      validator: (value, row) => {
-                        const response = { isValid: true, notification: { type: 'success', msg: '', title: '' }};
-                        if (!value) {
-                          response.isValid = false;
-                          response.notification.type = 'error';
-                          response.notification.msg = 'Value must be inserted';
-                          response.notification.title = '';
-                        }
-                        return response;
-                      }
-                    }}>
-                      <span onClick={() => {
-                        setFilter(item);
-                      }} style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                    return <TableHeaderColumn width="100" dataField={item} dataSort={ true }  editable={{validator: jobNameValidator}}>
+                      <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
                     </TableHeaderColumn>
                   }
 
-                  return <TableHeaderColumn width="100" dataField={item} >
-                    <span onClick={() => {
-                      setFilter(item);
-                    }} style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
+                  return <TableHeaderColumn width="100" dataField={item}  dataSort={ true }>
+                    <span  style={{cursor:'pointer'}}>{upperCasePipe(item)}</span>
                   </TableHeaderColumn>
                 })}
               </BootstrapTable>
