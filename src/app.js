b282716a2ca90565aa4202bf9f4277ad53e9efd7
Andriy Yurchuk <paltashka50@gmail.com>
2020-12-29 10:59:39 +0200

    add cards to skins

--- src/app.js
+++ src/app.js
@@ -6,26 +6,30 @@ import { configureStore } from "./redux/Store";
 import { History } from "./jwt/_helpers";
 import { PrivateRoute } from "./routes/PrivateRoutes";
 import BlankLayout from "./layouts/BlankLayout";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 const App = () => {
   //const [currentUser, SetcurrentUser] = useState(null);
   return (
-    <Provider store={configureStore()}>
-      <Router history={History}>
-        <Switch>
-          <Route exact path="/authentication/Login" component={BlankLayout} />;
-          {indexRoutes.map((prop, key) => {
-            return (
-              <PrivateRoute
-                path={prop.path}
-                key={key}
-                component={prop.component}
-              />
-            );
-          })}
-        </Switch>
-      </Router>
-    </Provider>
+    <DndProvider backend={HTML5Backend}>
+        <Provider store={configureStore()}>
+            <Router history={History}>
+                <Switch>
+                    <Route exact path="/authentication/Login" component={BlankLayout} />;
+                    {indexRoutes.map((prop, key) => {
+                        return (
+                            <PrivateRoute
+                                path={prop.path}
+                                key={key}
+                                component={prop.component}
+                            />
+                        );
+                    })}
+                </Switch>
+            </Router>
+        </Provider>
+    </DndProvider>
   );
 };
 
--- src/views/contacts/Contacts.js
+++ src/views/contacts/Contacts.js
@@ -4,55 +4,67 @@ import GeneralDetails from "../form-validation/FormValidation";
 import PCK from "../chat/Chat";
 import {getAllGamesThunk} from "../../reducers/games";
 import {useDispatch} from "react-redux";
-
+import Skins from "../Skins";
 
 
 export default () => {
+    const [isSelected, setSelected] = useState(false);
+    const [isGames, setGames] = useState(true)
+    const [isGeneral, setGeneral] = useState(false);
+    const [isSkins, setIsSkins] = useState(false);
+    const [isPCK, setPCK] = useState(false);
+    const [isRow, setRow] = useState(null)
+    const dispatch = useDispatch();
 
-const [isSelected, setSelected] = useState(false);
-const [isGames, setGames] = useState(true)
-const [isGeneral, setGeneral] = useState(false);
-const [isPCK, setPCK] = useState(false);
-const [isRow, setRow] = useState(null)
-const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getAllGamesThunk());
+    }, [])
 
-useEffect(() => {
-    dispatch(getAllGamesThunk());
-  }, [])
+    return (
+        <>
+            {
+                isSelected && <div>
+                    <button onClick={() => {
+                        setGeneral(true)
+                        setGames(false)
+                        setPCK(false)
+                        setIsSkins(false);
+                    }}>General details
+                    </button>
 
+                    <button onClick={() => {
+                        setGeneral(false)
+                        setGames(false)
+                        setPCK(false)
+                        setIsSkins(true);
+                    }}>Skins</button>
 
-  return (
-    <>
-      {
-       isSelected && <div>
-          <button onClick={() => {
-            setGeneral(true)
-            setGames(false)
-            setPCK(false)
-          }}>General details</button>
-
-          <button>Skins</button>
-
-          <button onClick={() => {
-            setPCK(true)
-            setGeneral(false)
-          }}>PCK</button>
-         <button onClick={() => {
-           setGeneral(false)
-           setGames(true)
-           setPCK(false)
-         }}>Games</button>
-        </div>
-      }
-      {
-        isGames &&  <AllGames setRow={setRow} setSelected={setSelected} />
-      }
-      {
-        isGeneral && <GeneralDetails isRow={isRow} />
-      }
-      {
-        isPCK && <PCK/>
-      }
-    </>
-  );
+                    <button onClick={() => {
+                        setPCK(true)
+                        setGames(false);
+                        setIsSkins(false);
+                        setGeneral(false)
+                    }}>PCK
+                    </button>
+                    <button onClick={() => {
+                        setGeneral(false)
+                        setGames(true)
+                        setPCK(false)
+                        setIsSkins(false);
+                    }}>Games
+                    </button>
+                </div>
+            }
+            {
+                isGames && <AllGames setRow={setRow} setSelected={setSelected}/>
+            }
+            {
+                isGeneral && <GeneralDetails isRow={isRow}/>
+            }
+            {isSkins && <Skins />}
+            {
+                isPCK && <PCK/>
+            }
+        </>
+    );
 };
